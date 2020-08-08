e361971dfc7654d06b680c429aa7f78bcda48966
Alif Munim <alif.munim@ryerson.ca>
2020-08-08 00:13:50 -0400

    Prepare for deployment
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- apollo-react/server.js
+++ apollo-react/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const schema = require("./schema");
+const path = require("path");
 
 const app = express();
 
@@ -13,7 +14,13 @@ app.use("/graphql", graphqlHTTP({
     graphiql: true
 }));
 
-const PORT = process.env.PORT || 5000
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`)
