056a6e902ec04bbcfc7484c6ef8ec5857564d82c
Alif Munim <alif.munim@ryerson.ca>
2020-08-12 22:19:26 -0400

    Fixes #23: Use react useState and useEffect hooks to dynamically render SinglePokemon
    Signed-off-by: Alif Munim <alif.munim@ryerson.ca>

--- graphql-pokemon/client/src/App.js
+++ graphql-pokemon/client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -9,13 +9,34 @@ const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
 });
 
+
 function App() {
+
+  const [displayNum, setDisplayNum] = useState(5);
+
   return (
     <ApolloProvider client={client}>
       <Router>
         <div>
           <h1 className="display-4 my-5 text-center">Pokemon</h1>
-          <Route exact path="/" component={MultiPokemon} />
+          <div className="container mb-5">
+            <input 
+              onChange={e=> {
+                setDisplayNum(parseInt(e.target.value))
+              }}
+              className="form-control" 
+              type="number" 
+              id="quantity" 
+              name="quantity" 
+              min="1" 
+              max="100"
+              value={displayNum}
+            />
+          </div>
+          <Route exact path="/" render={
+            (props) => 
+              <MultiPokemon {...props} num={displayNum}/>
+          } />
           <Route exact path="/pokemon/:name" component={PokemonInfo} />
         </div>
       </Router>
--- graphql-pokemon/client/src/components/MultiPokemon.js
+++ graphql-pokemon/client/src/components/MultiPokemon.js
@@ -1,28 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment, useEffect } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 import SinglePokemon from "./SinglePokemon";
 
 const MULTI_QUERY = gql`
-    query MultiPokemonQuery {
-        multiPokemon(limit: 20) {
+    query MultiPokemonQuery($num: Int!){
+        multiPokemon(limit: $num) {
             name
             url
         }
     }
 `;
 
-export default function MultiPokemon() {
+export default function MultiPokemon({
+    num
+}) {
+
+    useEffect(() => {
+        console.log("Number changed!")
+    }, [num]);
+
     return (
         <div>
-            <Query query={MULTI_QUERY}> 
+            <Query query={MULTI_QUERY} variables={{ num }}> 
                 {
                     ({loading, error, data}) => {
                         if (loading) return <h4>Loading...</h4>
                         if (error) console.log(error);
 
                         console.log(data.multiPokemon);
+                        console.log(num);
 
                         
                         return <div className="container">
@@ -32,8 +40,6 @@ export default function MultiPokemon() {
                                ))
                             }
                         </div>
-                        
-
                         
                     }
                 }
